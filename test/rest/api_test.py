b0d99dcef50bf4ef13a186f7b6c4183c3e45c3d7
Irene <66362460+irenegcantera@users.noreply.github.com>
2023-12-10 11:13:51 +0100

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -7,7 +7,9 @@ import pytest
 
 BASE_URL = "http://localhost:5000"
 BASE_URL_MOCK = "http://localhost:9090"
-DEFAULT_TIMEOUT = 10  # in secs
+DEFAULT_TIMEOUT = 2  # in secs
+MAX_RETRIES = 3
+RETRY_DELAY = 5
 
 @pytest.mark.api
 class TestApi(unittest.TestCase):
@@ -27,7 +29,19 @@ class TestApi(unittest.TestCase):
 
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        
+        for attempt in range(1, MAX_RETRIES + 1):
+            try:
+                response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            except urllib.error.URLError as e:
+                print(f"Attempt {attempt} failed: {e}")
+                if attempt < MAX_RETRIES:
+                    print(f"Retrying in {RETRY_DELAY} seconds...")
+                    sleep(RETRY_DELAY)
+                else:
+                    print(f"Max retries reached. Unable to establish a connection.")
+                    print(f"Failed to perform API request: {e}")
+                    
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
